69b8a5bec388bcbd1f59a3b2b446dde4dffe5a95
Michael Colenso <github@michaelcolenso.com>
2015-12-14 22:08:58 -0800

    update and make bundling work

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var bundler = require('aurelia-bundler');
-var bundles = require('../bundles.json');
+var gulp = require( "gulp" );
+var bundler = require( "aurelia-bundler" );
+var bundles = require( "../bundles.json" );
 
 var config = {
   force: true,
-  packagePath: '.',
+  packagePath: ".",
   bundles: bundles.bundles
 };
 
-gulp.task('bundle', function() {
-  return bundler.bundle(config);
-});
+gulp.task( "bundle", function() {
+  return bundler.bundle( config );
+} );
 
-gulp.task('unbundle', function() {
-  return bundler.unbundle(config);
-});
+gulp.task( "unbundle", function() {
+  return bundler.unbundle( config );
+} );
